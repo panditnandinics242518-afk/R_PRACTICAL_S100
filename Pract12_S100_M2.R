b31c8126c889fa1d6078dcf33be4af13ca968318
Nandini Pandit <panditnandinics242518@gmail.com>
2026-01-19 12:06:52 +0530

    Delete Pract12_S100_M2.R

--- Pract12_S100_M2.R
+++
@@ -1,9 +0,0 @@
-
-numeric_data <- iris_data[, c("SepalLengthCm", "SepalWidthCm",
-                              "PetalLengthCm", "PetalWidthCm")]
-
-# Correlation matrix
-cor_matrix <- cor(numeric_data)
-
-# Display correlation matrix
-print(cor_matrix)
